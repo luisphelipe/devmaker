15d56c7ab84d510c77fd34f50414cc49b868f2b1
luisphelipe <luisphelipecode@gmail.com>
2018-12-12 18:10:56 -0200

    Adiciona autenticação de API usando Passport
    Passport implementa Oauth2, apos instala-lo foi configurado roteamento
    e controller para autenticacao por API.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'PassportController@login');
+Route::post('signup', 'PassportController@signup');
+
+Route::group([
+    'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'PassportController@logout');
+        Route::get('user', 'PassportController@user');
 });
